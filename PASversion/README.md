b8bf5389a029438ffc15ea9aa8a322f13951f8a0
AnriTool <59344539+AnriTool@users.noreply.github.com>
2022-05-11 23:57:46 +1000

    +PASversion

---
+++ PASversion/README.md
@@ -0,0 +1,28 @@
+# GF ZPSKTR by AnriTool{RU}
+"Запускатор" Greenfoot игр, сохранённых в jar формате.
+
+## Принцип работы
+Код на паскале смотрит какие игры есть в папке "games" и показывает их в интерфейсе. При нажатии на "Start" создаётся .bat файл с нужными параметрами и он же запускается.
+
+## Важно
+* В папке jdk должен находится соответсвенно сам jdk(я брал всю папку прямиком из Standalone версии greenfoot)
+* В папке lib должен находится javafx(его я тоже брал с Standalone версии greenfoot)
+* В папке "games" должна быть хотябы одна игра в .jar формате(и только в нём, больше ничего в этой папке быть не должно)
+* Чтобы у игры было изображение, нужно чтобы название картинки соотвествовало названию игры, и картинка находилась в папке "images" 
+* Рекомендованное разрешение изображения: 178x130. Все другие размеры будут обрезаны до нужного разрешения.
+
+
+
+
+# GF ZPSKTR by AnriTool{EN}
+Launcher of Greenfoot games saved in jar format.
+
+## Working principle
+The pascal code looks at which games are in the "games" folder and shows them in the interface. When you click on "Start" it is created.a bat file with the necessary parameters and it also runs.
+
+## Important
+* The jdk folder should contain the jdk itself accordingly (I took the entire folder straight from the Standalone version of greenfoot)
+* javafx should be located in the lib folder (I also took it from the Standalone version of greenfoot)
+* There should be at least one game in the "games" folder.jar format (and only in it, nothing else should be in this folder)
+* In order for the game to have an image, it is necessary that the name of the picture corresponds to the name of the game, and the picture is in the "images" folder
+* Recommended image resolution: 178x130. All other sizes will be cropped to the desired resolution.
---
+++ PASversion/ZPSKTR.pas
@@ -0,0 +1,122 @@
+﻿{$apptype windows}
+{$reference 'System.Windows.Forms.dll'}
+{$reference 'System.Drawing.dll'}
+{$mainresource icon.res}
+Uses System ,System.IO, System.Windows.Forms, System.Drawing;
+
+
+
+var
+  bLeft, bRight, bStart :Button;
+  f : Form;
+  img: PictureBox;
+  gametxt :  System.Windows.Forms.Label;
+  counter: integer;
+  curGame: String;
+  var files := Directory.GetFiles('games\', '*.jar',SearchOption.AllDirectories);
+  
+procedure next(sender: object; e: EventArgs); 
+begin
+  files := Directory.GetFiles('games\', '*.jar',SearchOption.AllDirectories);
+  curGame := files[counter].ToString;
+  curGame := Copy(curGame,7,curGame.Length-10);
+  
+  if counter < files.Length-1 then
+    counter:= counter + 1
+  else counter := 0;  
+  curGame := files[counter].ToString;
+  curGame := Copy(curGame,7,curGame.Length-10);
+  if FileExists(Concat('images\', curGame, '.png')) then
+    img.Image:= Image.FromFile(Concat('images\', curGame, '.png'))
+  else img.Image:= Image.FromFile('images\NOIMAGE.png');
+  gametxt.Text := curGame+'.jar';
+  //writeln(curGame);
+end;
+
+procedure previous(sender: object; e: EventArgs); 
+begin
+  var files := Directory.GetFiles('games\', '*.jar',SearchOption.AllDirectories);
+  curGame := files[counter].ToString;
+  curGame := Copy(curGame,7,curGame.Length-10);
+  
+  if counter > 0 then
+    counter:= counter - 1
+  else counter := files.Length-1;  
+  curGame := files[counter].ToString;
+  curGame := Copy(curGame,7,curGame.Length-10);
+  if FileExists(Concat('images\', curGame, '.png')) then
+    img.Image:= Image.FromFile(Concat('images\', curGame, '.png'))
+  else img.Image:= Image.FromFile('images\NOIMAGE.png');
+  gametxt.Text := curGame+'.jar';
+  //writeln(curGame);
+end;
+
+procedure start(sender: object; e: EventArgs); 
+begin
+  //var cmd : string;
+  var fl: text;
+  assign(fl,'start.bat');
+  Rewrite(fl);
+  Write(fl,'jdk\bin\java.exe -cp "lib\javafx\lib\javafx.base.jar;lib\javafx\lib\javafx.controls.jarlib\javafx\lib\javafx.fxml.jar;lib\javafx\lib\javafx.media.jar;lib\javafx\lib\javafx.properties.jar;lib\javafx\lib\javafx.swing.jar;lib\javafx\lib\javafx.web.jar;GAMES\'+curGame+'.jar" --module-path "lib\javafx\lib" --add-modules=ALL-MODULE-PATH greenfoot.export.GreenfootScenarioApplication');
+  close(fl);
+  Exec('start.bat');
+end;
+
+begin
+
+counter := 0;
+//var files := Directory.GetFiles('games\', '*.jar',SearchOption.AllDirectories);
+curGame := files[counter].ToString;
+curGame := Copy(curGame,7,curGame.Length-10);
+//writeln(curGame);
+
+f:= new Form;
+f.Text := 'ZPSKTR';
+f.FormBorderStyle := System.Windows.Forms.FormBorderStyle.FixedSingle;
+f.Height := 215; f.Width := 324;
+f.Icon := new Icon('icon.ico'); 
+  
+bLeft := new Button;
+bLeft.Height := 26; bLeft.Width := 60; bLeft.Text := 'Previous';   
+bLeft.Top := 58; bLeft.Left := 5; bLeft.Click += previous;
+
+bRight := new Button;
+bRight.Height := 26; bRight.Width := 60; bRight.Text := 'Next';   
+bRight.Top := 58; bRight.Left := 254; bRight.Click += next;
+
+bStart := new Button;
+bStart.Height := 26; bStart.Width := 60; bStart.Text := 'Start';   
+bStart.Top := 145; bStart.Left := 188; bStart.Click += start;
+
+img := new PictureBox;
+img.Height := 130; img.Width := 178;
+img.Top := 5; img.Left := 70;
+if FileExists(Concat('images\', curGame, '.png')) then
+  img.Image:= Image.FromFile(Concat('images\', curGame, '.png'))
+else img.Image:= Image.FromFile('images\NOIMAGE.png');
+
+gametxt := new  System.Windows.Forms.Label();
+gametxt.Width := 120; gametxt.Height := 30;
+gametxt.Top := 142; gametxt.Left :=  62;
+
+gametxt.TextAlign := System.Drawing.ContentAlignment.MiddleRight;
+gametxt.Text := curGame+'.jar';
+
+  f.Controls.Add(bLeft);
+  f.Controls.Add(bRight);
+  f.Controls.Add(bStart);
+  f.Controls.Add(img);
+  f.Controls.Add(gametxt);
+  Application.Run(f);
+end.
+
+
+
+
+//var files := Directory.GetFiles('games\', '*.jar',SearchOption.AllDirectories);
+//var name:string;
+//for var i:=0 to files.Length-1 do begin
+  //name := files[i].ToString;
+  //name := Copy(name,7,name.Length-6);
+  //writeln(name);
+  //end;
